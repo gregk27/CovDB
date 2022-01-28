ecc4dbbaee458b75721ab4476e3d91343dd16b8c
Gregory Kelly <git@gregk.ca>
2022-01-28 13:18:38 -0500

    Create remaining tables/relationships

--- db.sql
+++ db.sql
@@ -31,8 +31,18 @@ CREATE TABLE Company (
 
 CREATE TABLE Site (
     name        VARCHAR(32) PRIMARY KEY,
-    -- TODO: Dates?
     address     VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE SiteDate (
+    site        VARCHAR(32) NOT NULL,
+    date        DATE        NOT NULL,
+
+    PRIMARY KEY (site, date),
+
+    FOREIGN KEY (site) REFERENCES Site(name)
+        ON UPDATE CASCADE -- If site name changes update relationshi
+        ON DELETE CASCADE -- If site removed update relationship
 );
 
 CREATE TABLE Lot (
@@ -45,11 +55,30 @@ CREATE TABLE Lot (
     site       VARCHAR(32),
 
     FOREIGN KEY (company)  REFERENCES Company(name)
-        ON UPDATE CASCADE -- If company name changes, update reference
-        ON DELETE RESTRICT -- Don't allow deletion of companies with lots
+        ON UPDATE CASCADE   -- If company name changes, update reference
+        ON DELETE RESTRICT, -- Don't allow deletion of companies with lots
     FOREIGN KEY (site)     REFERENCES Site(name)
-        ON UPDATE CASCADE -- If site name changes, update reference
-        ON DELETE RESTRICT -- Don't allow deletion of site with lots delivered
+        ON UPDATE CASCADE   -- If site name changes, update reference
+        ON DELETE RESTRICT  -- Don't allow deletion of site with lots delivered
+);
+
+CREATE TABLE Vaccination (
+    patient     VARCHAR(12) NOT NULL,
+    site        VARCHAR(32) NOT NULL,
+    lot         VARCHAR(6)  NOT NULL,
+    datetime    DATETIME    NOT NULL,
+
+    PRIMARY KEY (patient, site, lot),
+
+    FOREIGN KEY (patient) REFERENCES Patient(OHIP)
+        ON UPDATE CASCADE  -- Update vax record if patient updated
+        ON DELETE CASCADE, -- Delete vax record if patient deleted
+    FOREIGN KEY (site)  REFERENCES Site(name)
+        ON UPDATE CASCADE   -- Update vax record if site updated
+        ON DELETE RESTRICT, -- Prevent the deletion of sites if they have vaccinations
+    FOREIGN KEY (lot)   REFERENCES Lot(number)
+        ON UPDATE CASCADE  -- Update vax record if lot updated
+        ON DELETE RESTRICT -- Prevent the deletion of lots if they have vaccinations
 );
 
 CREATE TABLE Practice (
@@ -64,7 +93,7 @@ CREATE TABLE HealthcareWorker (
 );
 
 CREATE TABLE Nurse (
-    ID          INTEGER     NOT NULL,
+    ID          INTEGER     PRIMARY KEY,
 
     FOREIGN KEY (ID) REFERENCES HealthcareWorker(ID)
         ON UPDATE CASCADE -- Update if ID changed
@@ -72,7 +101,7 @@ CREATE TABLE Nurse (
 );
 
 CREATE TABLE Doctor (
-    ID          INTEGER     NOT NULL,
+    ID         INTEGER     PRIMARY KEY,
 
     practice   VARCHAR(32) NOT NULL,
 
@@ -84,9 +113,20 @@ CREATE TABLE Doctor (
         ON DELETE RESTRICT -- Prevent deletion of the practice if doctor exists
 );
 
-CREATE TABLE NurseWorksAt (
-    nurse       INTEGER NOT NULL,
-    site        VARCHAR NOT NULL,
+CREATE TABLE HCWCredential (
+    ID          INTEGER     NOT NULL,
+    credential  VARCHAR(4)  NOT NULL, -- How long are creds?
+
+    PRIMARY KEY (ID, credential),
+
+    FOREIGN KEY (ID) REFERENCES HealthcareWorker(ID)
+        ON UPDATE CASCADE -- If HCW ID changes update relationship
+        ON DELETE CASCADE -- If HCW removed update relationship
+);
+
+CREATE TABLE NurseWorksAt ( -- How to implement the participation constraint?
+    nurse       INTEGER     NOT NULL,
+    site        VARCHAR(32) NOT NULL,
 
     PRIMARY KEY (nurse, site),
 
@@ -94,20 +134,20 @@ CREATE TABLE NurseWorksAt (
         ON UPDATE CASCADE -- If nurse ID changes update relationship
         ON DELETE CASCADE,-- If nurse removed update relationship
     FOREIGN KEY (site) REFERENCES Site(name)
-        ON UPDATE CASCADE -- If site ID changes update relationship
+        ON UPDATE CASCADE -- If site name changes update relationship
         ON DELETE CASCADE -- If site removed update relationship
-)
+);
 
 CREATE TABLE DoctorWorksAt (
-    doctor      INTEGER NOT NULL,
-    site        VARCHAR NOT NULL,
+    doctor      INTEGER     NOT NULL,
+    site        VARCHAR(32) NOT NULL,
 
     PRIMARY KEY (doctor, site),
 
-    FOREIGN KEY (doctor) REFERENCES Durse(ID)
+    FOREIGN KEY (doctor) REFERENCES Doctor(ID)
         ON UPDATE CASCADE -- If doctor ID changes update relationship
         ON DELETE CASCADE,-- If doctor removed update relationship
     FOREIGN KEY (site) REFERENCES Site(name)
-        ON UPDATE CASCADE -- If site ID changes update relationship
+        ON UPDATE CASCADE -- If site name changes update relationship
         ON DELETE CASCADE -- If site removed update relationship
-)
+);
