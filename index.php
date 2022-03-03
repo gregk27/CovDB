da938bbc01e4c73b1157a4b7c9e764dc59a38f15
Gregory Kelly <git@gregk.ca>
2022-03-03 10:42:19 -0500

    Create basic landing page

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo "Hello World!";
+echo $_SERVER["REQUEST_URI"];
 phpinfo();
--- public/covid.php
+++ public/covid.php
@@ -9,5 +9,26 @@
     <script src='main.js'></script>
 </head>
 <body>
+    <header>
+        <h1>Covid Vaccination Database</h1>
+    </header>
+    <section class="panelgrid">
+        <div class="panel clickable">
+            <h2>Record Vaccination</h2>
+            <p>Submit a record of your vaccination</p>
+        </div>
+        <div class="panel clickable">
+            <h2>Search for Vaccine</h2>
+            <p>Search sites to find a vaccine</p>
+        </div>
+        <div class="panel clickable">
+            <h2>Check Status</h2>
+            <p>Check your vaccination status</p>
+        </div>
+        <div class="panel clickable">
+            <h2>Worker Lookup</h2>
+            <p>See who's working at a site</p>
+        </div>
+    </section>
 </body>
 </html>
