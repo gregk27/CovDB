c5e4ea29c829c82dcf5f624a94a0ca8adf34e2a2
Gregory Kelly <git@gregk.ca>
2022-03-03 11:13:25 -0500

    Add icons with fontawesome

--- public/covid.php
+++ public/covid.php
@@ -6,6 +6,8 @@
     <title>COVID DB</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <!-- Inlcude FontAwesome for icons -->
+    <script src="https://kit.fontawesome.com/60394e1dab.js" crossorigin="anonymous"></script>
     <script src='main.js'></script>
 </head>
 <body>
@@ -14,18 +16,22 @@
     </header>
     <section class="panelgrid">
         <div class="panel clickable">
+            <i class="fa-solid fa-file-medical"></i>
             <h2>Record Vaccination</h2>
             <p>Submit a record of your vaccination</p>
         </div>
         <div class="panel clickable">
+            <i class="fa-solid fa-magnifying-glass"></i>
             <h2>Search for Vaccine</h2>
             <p>Search sites to find a vaccine</p>
         </div>
         <div class="panel clickable">
+            <i class="fa-solid fa-syringe"></i>
             <h2>Check Status</h2>
             <p>Check your vaccination status</p>
         </div>
         <div class="panel clickable">
+            <i class="fa-solid fa-user-doctor"></i>
             <h2>Worker Lookup</h2>
             <p>See who's working at a site</p>
         </div>
